d650d28c4f5747c57a6b7004ad049eb92c9fb49a
Kevin Wenger <wenger.kev@gmail.com>
2015-12-28 23:03:06 +0100

    detail of works

--- modules/custom/amicolor/works/assets/js/works.js
+++ modules/custom/amicolor/works/assets/js/works.js
@@ -8,11 +8,20 @@
         $(document).on('mouseleave', '.works_block .works_list .work', function(){
             $(this).find('.work-hover').animate({opacity: 0}, 150);
         });
-        // $(document).on('click', '.works_block .works_list .work-hover .inner .call-to-action', function(){
-        //     $(this).find('.work-hover').animate({opacity: 0}, 150);
-        // });
-        $('.works_block .works_list .work-hover .inner .call-to-action').magnificPopup({
-            type: 'ajax'
+
+        $(document).on('click', '.works_block .works_list .work-hover .inner .call-to-action, .mfp-content .works_detail .works_detail-prev, .mfp-content .works_detail .works_detail-next', function(event){
+            event.preventDefault();
+            var url = $(this).attr('href');
+
+            if( $(this).hasClass('disabled') == false ){
+                $.magnificPopup.open({
+                  type: 'ajax',
+                  items: {
+                      src: url
+                  },
+                }, 0);
+            }
         });
+
     });
 })(jQuery, Drupal, window);
